0294f8fd09225f765656610cc320a2a58aa9a40c
zusiyuuki <zusiyuuki@gmail.com>
2022-08-29 18:20:20 +0900

    変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar/navbar.component';
-import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
